2ba9732acf1ca91319489a06da094414b449e0c1
Ivan Trubach <mr.trubach@icloud.com>
2019-12-01 01:16:06 +0300

    Split files.pack to base/server/client

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
-modpacker compile -o server.zip base/*.pack files/*.pack server/*.pack
-modpacker compile -o client.zip base/*.pack files/*.pack client/*.pack
+modpacker compile -o server.zip base/*.pack server/*.pack
+modpacker compile -o client.zip base/*.pack client/*.pack
