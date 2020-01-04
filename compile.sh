6a76cc0445cf81f356dce4c21664a75131870a8b
Ivan Trubach <mr.trubach@icloud.com>
2020-01-04 05:51:55 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
-modpacker compile -o server.zip base/*.pack server/*.pack
-modpacker compile -o client.zip base/*.pack client/*.pack
+modpacker compile -o server.zip base/*.pack base/sucks/*.pack server/*.pack
+modpacker compile -o client.zip base/*.pack base/sucks/*.pack client/*.pack
